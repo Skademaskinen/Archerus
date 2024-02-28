9a4393d9826054ddb4bfbdf3999de4f0ff52146c
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-02-28 09:49:54 +0100

    fixed paths in skademaskinen config

--- systems/skademaskinen.nix
+++ systems/skademaskinen.nix
@@ -1,8 +1,8 @@
 {
     imports =[
-        ./hardware-configuration.nix
-        ./users
-        ./services
-        ./setup
+        ../hardware-configuration.nix
+        ../users
+        ../services
+        ../setup
     ];
 }
