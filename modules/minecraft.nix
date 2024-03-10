e38dd786f6ad8110a9ef1a311702e5ef69438ae4
Mast3r_waf1z <32744171+Mast3rwaf1z@users.noreply.github.com>
2024-03-10 23:45:31 +0100

    rewrote minecraft config (#15)

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -1,5 +1,41 @@
-{ config, lib, pkgs, modulesPath, ... }: {
+{ config, lib, pkgs, ... }: let
+    prefix = "/mnt/raid/minecraft";
+    paperSource = pkgs.fetchurl {
+        url = "https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/450/downloads/paper-1.20.4-450.jar";
+        sha256 = "sha256-SHqHHe0+utxdv2wfZOeikBNATfcHVJ5fMEL4jPXmopU=";
+    };
+    waterfallSource = pkgs.fetchurl {
+        url = "https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/565/downloads/waterfall-1.20-565.jar";
+        sha256 = "sha256-PSdqP1f4UDx+VIhZ0PxrxBfZZyEW2fp+6wD3YQcH1vo=";
+    };
 
+    makeServer = {waterfall ? false, name}: {
+        enable = true;
+        description = "Minecraft ${name} Service";
+        serviceConfig = {
+            WorkingDirectory = "${prefix}/${name}";
+            ExecStart = if waterfall
+                then "${pkgs.jdk}/bin/java -jar ${waterfallSource}"
+                else "${pkgs.jdk}/bin/java -jar ${paperSource}";
+            Restart = "on-failure";
+            StandardInput = "socket";
+            StandardOutput = "journal";
+            StandardError = "journal";
+            Sockets = "minecraft-${name}.socket";
+        };
+        wantedBy = [ "default.target" ];
+    };
+    makeSocket = {name}: {
+        enable = true;
+        description = "Minecraft ${name} STDIN socket";
+        socketConfig = {
+            ListenFIFO = "${prefix}/${name}/${name}.stdin";
+            Service = "minecraft-${name}.service";
+            User = "minecraft";
+        };
+    };
+
+in {
     users.users.minecraft = {
         isSystemUser = true;
         description = "minecraft server manager";
@@ -8,137 +44,20 @@
     };
     users.groups.minecraft = { };
 
-    systemd.services.minecraft-waterfall = {
-        enable = true;
-        description = "minecraft waterfall service";
-        serviceConfig = {
-            WorkingDirectory = "/mnt/raid/minecraft/waterfall";
-            User = "minecraft";
-            ExecStart =
-                "${pkgs.jdk}/bin/java -jar /mnt/raid/minecraft/waterfall/waterfall.jar";
-            Restart = "on-failure";
-            StandardInput = "socket";
-            StandardOutput = "journal";
-            StandardError = "journal";
-            Sockets = "minecraft-waterfall.socket";
-        };
-        wantedBy = [ "default.target" ];
+    systemd.services = {
+        minecraft-waterfall =   makeServer { waterfall = true; name = "waterfall"; };
+        minecraft-survival =    makeServer { name = "survival"; };
+        minecraft-hub =         makeServer { name = "hub"; };
+        minecraft-creative =    makeServer { name = "creative"; };
+        minecraft-paradox =     makeServer { name = "paradox"; };
     };
+    systemd.sockets = {
+        minecraft-waterfall =   makeSocket { name = "waterfall"; };
+        minecraft-survival =    makeSocket { name = "survival"; };
+        minecraft-hub =         makeSocket { name = "hub"; };
+        minecraft-creative =    makeSocket { name = "creative"; };
+        minecraft-paradox =     makeSocket { name = "paradox"; };
 
-    systemd.services.minecraft-survival = {
-        enable = true;
-        description = "minecraft survival service";
-        serviceConfig = {
-            WorkingDirectory = "/mnt/raid/minecraft/survival";
-            User = "minecraft";
-            ExecStart =
-                "${pkgs.jdk}/bin/java -jar /mnt/raid/minecraft/survival/survival.jar";
-            Restart = "on-failure";
-            StandardInput = "socket";
-            StandardOutput = "journal";
-            StandardError = "journal";
-            Sockets = "minecraft-survival.socket";
-        };
-        wantedBy = [ "default.target" ];
     };
-
-    systemd.services.minecraft-hub = {
-        enable = true;
-        description = "minecraft hub service";
-        serviceConfig = {
-            WorkingDirectory = "/mnt/raid/minecraft/hub";
-            User = "minecraft";
-            ExecStart = "${pkgs.jdk}/bin/java -jar /mnt/raid/minecraft/hub/hub.jar";
-            Restart = "on-failure";
-            StandardInput = "socket";
-            StandardOutput = "journal";
-            StandardError = "journal";
-            Sockets = "minecraft-hub.socket";
-        };
-        wantedBy = [ "default.target" ];
-    };
-
-    systemd.services.minecraft-creative = {
-        enable = true;
-        description = "minecraft creative service";
-        serviceConfig = {
-            WorkingDirectory = "/mnt/raid/minecraft/creative";
-            User = "minecraft";
-            ExecStart =
-                "${pkgs.jdk}/bin/java -jar /mnt/raid/minecraft/creative/creative.jar";
-            Restart = "on-failure";
-            StandardInput = "socket";
-            StandardOutput = "journal";
-            StandardError = "journal";
-            Sockets = "minecraft-creative.socket";
-        };
-        wantedBy = [ "default.target" ];
-    };
-
-    systemd.services.minecraft-paradox = {
-        enable = true;
-        description = "Minecraft paradox service";
-        serviceConfig = {
-            WorkingDirectory = "/mnt/raid/minecraft/paradox";
-            User = "minecraft";
-            ExecStart =
-                "${pkgs.jdk}/bin/java -jar /mnt/raid/minecraft/paradox/paradox.jar";
-            Restart = "on-failure";
-            StandardInput = "socket";
-            StandardOutput = "journal";
-            StandardError = "journal";
-            Sockets = "minecraft-paradox.socket";
-        };
-        wantedBy = [ "default.target" ];
-    };
-
-    systemd.sockets.minecraft-waterfall = {
-        enable = true;
-        description = "Minecraft waterfall STDIN socket";
-        socketConfig = {
-            ListenFIFO = "%t/waterfall.stdin";
-            Service = "minecraft-waterfall.service";
-            User = "minecraft";
-        };
-    };
-
-    systemd.sockets.minecraft-survival = {
-        enable = true;
-        description = "Minecraft survival STDIN socket";
-        socketConfig = {
-            ListenFIFO = "%t/survival.stdin";
-            Service = "minecraft-survival.service";
-            User = "minecraft";
-        };
-    };
-
-    systemd.sockets.minecraft-hub = {
-        enable = true;
-        description = "Minecraft hub STDIN socket";
-        socketConfig = {
-            ListenFIFO = "%t/hub.stdin";
-            Service = "minecraft-hub.service";
-            User = "minecraft";
-        };
-    };
-
-    systemd.sockets.minecraft-creative = {
-        enable = true;
-        description = "Minecraft creative STDIN socket";
-        socketConfig = {
-            ListenFIFO = "%t/creative.stdin";
-            Service = "minecraft-creative.service";
-            User = "minecraft";
-        };
-    };
-
-    systemd.sockets.minecraft-paradox = {
-        enable = true;
-        description = "Minecraft paradox STDIN socket";
-        socketConfig = {
-            ListenFIFO = "%t/paradox.stdin";
-            Service = "minecraft-paradox.service";
-            User = "minecraft";
-        };
-    };
+    
 }   
