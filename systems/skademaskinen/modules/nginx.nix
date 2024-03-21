d0ebe8a9ea6612a8df4fa2ccf13c87e9e5f60ffc
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-03-21 02:02:30 +0100

    fixed paths for proxys

--- systems/skademaskinen/modules/nginx.nix
+++ systems/skademaskinen/modules/nginx.nix
@@ -18,8 +18,8 @@
         virtualHosts."document.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:8123";
         virtualHosts."jupyter.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.services.jupyterhub.port}";
         virtualHosts."nextcloud.${config.skademaskinen.domain}" = makeProxy "/*" "http://localhost:${builtins.toString config.services.nextcloud.extraOptions.port}";
-        virtualHosts."website.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.skademaskinen.mast3r.website.port}";
-        virtualHosts."taoshi.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.skademaskinen.taoshi.website.port}";
+        virtualHosts."mast3r.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.skademaskinen.mast3r.website.port}";
+        virtualHosts."taoshi.${config.skademaskinen.domain}" = makeProxy "/*" "http://localhost:${builtins.toString config.skademaskinen.taoshi.website.port}";
         virtualHosts."${config.skademaskinen.domain}" = {
             inherit sslCertificate;
             inherit sslCertificateKey;
