43930c09c09861665648a42731d6dc66f5d53a3e
mast3r <thomasmollerjensen@live.dk>
2024-03-21 12:01:53 +0100

    rewrite jupyter

--- systems/skademaskinen/modules/nginx.nix
+++ systems/skademaskinen/modules/nginx.nix
@@ -25,11 +25,10 @@
             inherit sslCertificate;
             inherit sslCertificateKey;
             forceSSL = true;
-            locations."/*" = {
+            locations."/" = {
                 proxyPass = "http://localhost:${builtins.toString config.services.jupyterhub.port}";
                 proxyWebsockets = true;
                 extraConfig = ''
-                    proxy_redirect   off;
                     proxy_set_header X-Real-IP $remote_addr;
                     proxy_set_header Host $host;
                     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
