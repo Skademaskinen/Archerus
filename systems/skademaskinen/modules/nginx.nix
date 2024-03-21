4972f7db5f7490f0de79a00f8159da5c11e0f250
mast3r <thomasmollerjensen@live.dk>
2024-03-21 12:04:52 +0100

    rewrite jupyter

--- systems/skademaskinen/modules/nginx.nix
+++ systems/skademaskinen/modules/nginx.nix
@@ -3,15 +3,11 @@
     services.nginx = let 
         sslCertificate = "/opt/SSL/domain.cert.pem";
         sslCertificateKey = "/opt/SSL/private.key.pem";
-        makeProxy = location: {
+        makeProxy = path: location: {
             inherit sslCertificate;
             inherit sslCertificateKey;
             forceSSL = true;
-            locations."/" = {
-                proxyPass = location;
-                proxyWebsockets = true;
-            };
-            locations."/*" = {
+            locations.${path} = {
                 proxyPass = location;
                 proxyWebsockets = true;
             };
@@ -22,11 +18,11 @@
         recommendedGzipSettings = true;
         recommendedTlsSettings = true;
 
-        virtualHosts."document.${config.skademaskinen.domain}" = makeProxy "http://localhost:8123";
-        virtualHosts."nextcloud.${config.skademaskinen.domain}" = makeProxy "http://localhost:${builtins.toString config.services.nextcloud.extraOptions.port}";
-        virtualHosts."api.${config.skademaskinen.domain}" = makeProxy "http://localhost:${builtins.toString config.skademaskinen.mast3r.website.port}";
-        virtualHosts."taoshi.${config.skademaskinen.domain}" = makeProxy "http://localhost:${builtins.toString config.skademaskinen.taoshi.website.port}";
-        virtualHosts."jupyter.${config.skademaskinen.domain}" = makeProxy "http://localhost:${builtins.toString config.services.jupyterhub.port}";
+        virtualHosts."document.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:8123";
+        virtualHosts."jupyter.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.services.jupyterhub.port}";
+        virtualHosts."nextcloud.${config.skademaskinen.domain}" = makeProxy "/*" "http://localhost:${builtins.toString config.services.nextcloud.extraOptions.port}";
+        virtualHosts."api.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.skademaskinen.mast3r.website.port}";
+        virtualHosts."taoshi.${config.skademaskinen.domain}" = makeProxy "/" "http://localhost:${builtins.toString config.skademaskinen.taoshi.website.port}";
         virtualHosts."${config.skademaskinen.domain}" = {
             inherit sslCertificate;
             inherit sslCertificateKey;
