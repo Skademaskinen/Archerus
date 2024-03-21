993cae5f0d78fdf4d0adf0f5f817623f37572c29
Taoshi <nicolaikh@live.dk>
2024-03-21 17:08:56 +0100

    update sketch bot again

--- packages/sketch-bot/default.nix
+++ packages/sketch-bot/default.nix
@@ -6,8 +6,8 @@ in pkgs.buildDotnetModule {
     src = pkgs.fetchFromGitHub {
         owner = "Taoshix";
         repo = name;
-        rev = "69801734762c66967f1a53148d970d68d5e34576";
-        sha256 = "sha256-aqNposVOc2droqeaBOAafS3Dd7aFDeV0Wy7pcvueP8Y=";
+        rev = "8f6c2b951adc3659ba192d0a5463acce046f499d";
+        sha256 = "1gg1q72r25pyipf5q3v9p6vammwkyy7g2ny9fw79mlbsxyniswf1";
     };
     dotnet-sdk = pkgs.dotnet-sdk_8;
     dotnet-runtime = pkgs.dotnet-runtime_8;
