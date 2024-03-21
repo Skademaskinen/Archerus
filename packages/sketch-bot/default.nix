77b02e9d555ccd2cf3ab65f564c4944956d957e1
mast3r <thomasmollerjensen@live.dk>
2024-03-21 15:07:21 +0100

    sketch bot builds

--- packages/sketch-bot/default.nix
+++ packages/sketch-bot/default.nix
@@ -6,8 +6,8 @@ in pkgs.buildDotnetModule {
     src = pkgs.fetchFromGitHub {
         owner = "Taoshix";
         repo = name;
-        rev = "8013adbf744cc79639c6a9ee341d6ae0af19379e";
-        sha256 = "sha256-F221KPsd6qbOyEsAYFSwTNPETnjBxblYxDoPu8klRrM=";
+        rev = "e642da039d4a5f42259a142d8d710f22aeb23d34";
+        sha256 = "sha256-EZmpJaGlLXed45YPZl2lIyQYxNV6eQ2tv8Afi7uDW/Y=";
     };
     dotnet-sdk = pkgs.dotnet-sdk_8;
     dotnet-runtime = pkgs.dotnet-runtime_8;
