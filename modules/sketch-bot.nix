66000413ca55fbb69db2a30017c42b82ef522eac
mast3r <thomasmollerjensen@live.dk>
2024-03-21 15:09:53 +0100

    sketch bot builds!

--- modules/sketch-bot.nix
+++ modules/sketch-bot.nix
@@ -9,14 +9,14 @@
         };
     };
     config.systemd.services.sketch-bot = if config.skademaskinen.sketch-bot.enable then let
-        #pkg = pkgs.callPackage ../packages/sketch-bot {};
+        pkg = pkgs.callPackage ../packages/sketch-bot {};
     in {
         enable = true;
         description = "SketchBot service";
         serviceConfig = {
             WorkingDirectory = config.skademaskinen.sketch-bot.root;
             User = "taoshi";
-            ExecStart = "${pkgs.dotnet-sdk_8}/bin/dotnet run";
+            ExecStart = "${pkg}/bin/SketchBot";
         };
         wantedBy = [ "default.target" ];
         after = [ "network-online.target" ];
