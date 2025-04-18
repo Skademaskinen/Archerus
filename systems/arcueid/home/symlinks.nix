3ce4fc492af30f0268e450a7fa50c6e0198c7ece
Thomas Jensen <mast3r@skade.dev>
2025-04-18 03:12:24 +0200

    initial desktop readded

---
+++ systems/arcueid/home/symlinks.nix
@@ -0,0 +1,9 @@
+ {
+    home.file = {
+        Documents.source = /data/files/Documents;
+        Pictures.source = /data/files/Pictures;
+        Downloads.source = /data/files/Downloads;
+        git.source = /data/files/git;
+        Games.source = /data/games;
+    };
+ }
