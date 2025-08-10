cd28ed58b27591702fd6797c2e8c560ccb9aefa0
Thomas Jensen <32744171+Mast3rwaf1z@users.noreply.github.com>
2025-08-10 12:06:43 +0200

    hwconfig (#78)

--- systems/skademaskinen/hardware-configuration.nix
+++ systems/skademaskinen/hardware-configuration.nix
@@ -1,3 +1,30 @@
-{
+{ lib, config, ... }:
 
+{
+    # hardware
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    boot.swraid.enable = true;
+    boot.swraid.mdadmConf = "MAILADDR mast3r@${config.skademaskinen.domain}";
+
+    fileSystems = {
+        "/" = { 
+            device = "/dev/disk/by-label/ROOT";
+            fsType = "ext4";
+        };
+        "/boot" = {
+            device = "/dev/disk/by-label/BOOT";
+            fsType = "vfat";
+        };
+        "${config.skade.projectsRoot}" = {
+            device = "/dev/disk/by-label/STORAGE";
+            fsType = "ext4";
+        };
+    };
+    swapDevices = [{ device="/dev/disk/by-uuid/3aac8229-8ab1-4c0d-a2c6-d27859553817"; }];
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
