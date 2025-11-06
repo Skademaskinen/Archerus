46ae2c79328a7311d1a45092ede69b77bb5b0794
= <mast3r@skade.dev>
2025-11-06 19:08:31 +0100

    stuff

--- modules/homeManagerModules/neovim/lsp-cmp.nix
+++ modules/homeManagerModules/neovim/lsp-cmp.nix
@@ -1,81 +1,88 @@
 { pkgs, ... }:
 
 {
-    programs.nixvim.plugins = {
-        lsp = {
-            enable = true;
-            inlayHints = true;
-            servers = {
-                pyright.enable = true;
-                nixd.enable = true;
-                hls = {
-                    enable = true;
-                    installGhc = false;
+    programs.nixvim = {
+        plugins = {
+            lsp = {
+                enable = true;
+                inlayHints = true;
+                servers = {
+                    pyright.enable = true;
+                    nixd.enable = true;
+                    hls = {
+                        enable = true;
+                        installGhc = false;
+                    };
+                    cmake.enable = true;
+                    clangd.enable = true;
                 };
-                cmake.enable = true;
-                clangd.enable = true;
             };
-        };
 
-        tiny-inline-diagnostic = {
-            enable = true;
-            settings = {
-                preset = "powerline";
-                options = {
-                    show_all_diags_on_cursorline = true;
-                    multilines = {
-                        enabled = true;
-                        always_show = true;
+            tiny-inline-diagnostic = {
+                enable = true;
+                settings = {
+                    preset = "powerline";
+                    options = {
+                        show_all_diags_on_cursorline = true;
+                        multilines = {
+                            enabled = true;
+                            always_show = true;
+                        };
                     };
                 };
             };
-        };
 
-        lsp-signature = {
-            enable = true;
-            settings = {
-                extra_trigger_chars = [
-                    "("
-                    ","
-                ];
-                hint_prefix = {
-                    above = "↙ ";
-                    below = "↖ ";
-                    current = "← ";
+            lsp-signature = {
+                enable = true;
+                settings = {
+                    extra_trigger_chars = [
+                        "("
+                        ","
+                    ];
+                    hint_prefix = {
+                        above = "↙ ";
+                        below = "↖ ";
+                        current = "← ";
+                    };
+                    padding = " ";
+                    shadow_guibg = "#121315";
+                    toggle_key = "<M-x>";
+                    floating_window = false;
                 };
-                padding = " ";
-                shadow_guibg = "#121315";
-                toggle_key = "<M-x>";
-                floating_window = false;
             };
-        };
 
-        cmp = {
-            enable = true;
-            autoEnableSources = true;
-            settings.sources = [
-                { name = "nvim_lsp"; priority = 400; }
-                { name = "path"; priority = 300; }
-                { name = "buffer"; priority = 200; }
-            ];
-            settings.mapping = {
-                "<C-Space>" = "cmp.mapping.complete()";
-                "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-                "<C-e>" = "cmp.mapping.close()";
-                "<C-f>" = "cmp.mapping.scroll_docs(4)";
-                "<CR>" = "cmp.mapping.confirm({ select = true })";
-                "<C-Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-                "<C-Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            cmp = {
+                enable = true;
+                autoEnableSources = true;
+                settings.sources = [
+                    { name = "nvim_lsp"; priority = 400; }
+                    { name = "path"; priority = 300; }
+                    { name = "buffer"; priority = 200; }
+                ];
+                settings.mapping = {
+                    "<C-Space>" = "cmp.mapping.complete()";
+                    "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                    "<C-e>" = "cmp.mapping.close()";
+                    "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                    "<CR>" = "cmp.mapping.confirm({ select = true })";
+                    "<C-Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+                    "<C-Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                };
             };
+
+            trouble = {
+                enable = true;
+            };
+
+            #haskell-scope-highlighting = {
+            #    enable = true;
+            #};
         };
 
-        trouble = {
-            enable = true;
-        };
+        extraPlugins = [
+            pkgs.vimPlugins.haskell-tools-nvim
+        ];
 
-        haskell-scope-highlighting = {
-            enable = true;
-        };
     };
 
 }
